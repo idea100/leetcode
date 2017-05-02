289a272cd9e74ee5b4ea3477ccf38baac64ca7dd
idea100 <517626980@qq.com>
2017-05-02 11:07:04 +0800

    modify

--- leetcode/item1.js
+++ leetcode/item1.js
@@ -13,15 +13,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    var map = {}
-    for (var i = 0, length = nums.length; i < length; i++) {
-        var num = nums[i]
-        if (map[num] !== undefined) {
-            return [map[target-num], i]
-        }
-        map[num] = i
-        map[target - num] = true
-    }
-    return []
-};
+ var twoSum = function(nums, target) {
+     var map = {}
+     for (var i = 0, length = nums.length; i < length; i++) {
+         var num = nums[i]
+         if (map[target-num] !== undefined) {
+             return [map[target-num], i]
+         }
+         map[num] = i
+
+     }
+     return []
+ };
